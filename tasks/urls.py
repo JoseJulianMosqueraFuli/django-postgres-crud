4ca9777c9e6afcf68ba55d0944449488af1ce93b
Jose Mosquera-Linux <jojumofu@gmail.com>
2023-02-09 20:52:37 -0500

    update: add path to recieve task from front

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import list_task
+from .views import list_task, create_task
 
 urlpatterns = [
     path('', list_task),
+    path('new/',create_task)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def list_task(request):
     return render(request,'list_task.html')
+
+def create_task(request):
+    print(request.POST)
+    return redirect('/tasks/')
