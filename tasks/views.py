74d4f4d0026a0803c3eef6181fd5dd68f7f71eb9
Jose Mosquera-Linux <jojumofu@gmail.com>
2023-02-10 17:25:07 -0500

    update: show list of task in frontend

--- tasks/views.py
+++ tasks/views.py
@@ -3,10 +3,13 @@ from .models import Task
 
 # Create your views here.
 def list_task(request):
-    return render(request,'list_task.html')
+    tasks = Task.objects.all()
+    print(tasks)
+    return render(request,'list_task.html', {'tasks': tasks})
 
 def create_task(request):
     print(request.POST)
     task = Task(tittle=request.POST['tittle'], description=request.POST['description'])
     task.save()
     return redirect('/tasks/')
+
